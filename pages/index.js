c6d32e300d07e40aec16f24b28b435e32eb3e64f
Bakytzhan Baizhikenov <bahakz@Bakytzhans-iMac.local>
2017-10-25 13:33:23 +0600

    feat: initial version

---
+++ pages/index.js
@@ -0,0 +1,85 @@
+import Link from "next/link";
+import Head from "../components/head";
+import App from "grommet/components/App";
+import Article from "grommet/components/Article";
+import Header from "grommet/components/Header";
+import Section from "grommet/components/Section";
+import Hero from "grommet/components/Hero";
+import Headline from "grommet/components/Headline";
+import Title from "grommet/components/Title";
+import Video from "grommet/components/Video";
+import Image from "grommet/components/Image";
+import Heading from "grommet/components/Heading";
+import Search from "grommet/components/Search";
+import Anchor from "grommet/components/Anchor";
+import Box from "grommet/components/Box";
+import Footer from "grommet/components/Footer";
+import CirclePlayIcon from "grommet/components/icons/base/CirclePlay";
+import DocHead from "components/head";
+import Logo from "../components/Logo";
+
+export default () => (
+  <App centered={false}>
+    <DocHead />
+
+    <Header size="small" colorIndex="light-2">
+      <Box>
+        <Logo />
+      </Box>
+    </Header>
+
+    <Hero
+      background={
+        <Video
+          autoPlay={true}
+          showControls={false}
+          loop={true}
+          muted={true}
+          fit="cover"
+          style={{ opacity: 0.8 }}
+        >
+          <source src="/static/video/promovideo.mp4" type="video/mp4" />
+        </Video>
+      }
+      size="large"
+      style={{ height: "100%", backgroundColor: "black" }}
+      backgroundColorIndex="dark"
+    >
+      <Box
+        margin="none"
+        pad="large"
+        direction="row"
+        align="stretch"
+        alignSelf="stretch"
+        basis="full"
+      >
+        <Box basis="1/3" />
+        <Box direction="column" justify="center" align="center" basis="1/3">
+          <Heading margin="50px" uppercase="true" strong="true">
+            VR FIT
+          </Heading>
+          <Headline margin="50px" align="center" size="small">
+            Cinematic 360 VR rowing experience with elite teams and beautiful
+            locations. Travel and get fit from the comfort of your home
+          </Headline>
+          <CirclePlayIcon size="large" colorIndex="white" />
+        </Box>
+      </Box>
+    </Hero>
+
+    <Footer
+      primary={true}
+      appCentered={true}
+      direction="column"
+      align="center"
+      pad="small"
+    >
+      <p>
+        Created by {" "}
+        <Anchor href="https://intellectionstudio.com" target="_blank">
+          Intellection Studio
+        </Anchor>
+      </p>
+    </Footer>
+  </App>
+);
